6a07ae2f39c5ed6baf566310005ee0f793f76a2e
prashanthdkumar <prashanthdkumar88@gmail.com>
2020-07-14 12:50:04 -0400

    added configuration for firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyA1Syrg6nNmTnWplD89vhek1ZY-jalKBUw",
+    authDomain: "oshop-44cfe.firebaseapp.com",
+    databaseURL: "https://oshop-44cfe.firebaseio.com",
+    projectId: "oshop-44cfe",
+    storageBucket: "oshop-44cfe.appspot.com",
+    messagingSenderId: "811787140787",
+    appId: "1:811787140787:web:6b17a80f9b5848b50a3ac6",
+    measurementId: "G-P3KWT2NZE6"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA1Syrg6nNmTnWplD89vhek1ZY-jalKBUw",
+    authDomain: "oshop-44cfe.firebaseapp.com",
+    databaseURL: "https://oshop-44cfe.firebaseio.com",
+    projectId: "oshop-44cfe",
+    storageBucket: "oshop-44cfe.appspot.com",
+    messagingSenderId: "811787140787",
+    appId: "1:811787140787:web:6b17a80f9b5848b50a3ac6",
+    measurementId: "G-P3KWT2NZE6"
+  }
 };
 
 /*
