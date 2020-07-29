2b85ea7813ea88cadd98ce486279dcb4c92c7c1e
prashanthdkumar <prashanthdkumar88@gmail.com>
2020-07-29 16:29:55 -0400

    Showing or Hiding the Admin Links

--- src/app/admin-auth-guard.service.ts
+++ src/app/admin-auth-guard.service.ts
@@ -2,7 +2,6 @@ import { UserService } from './user.service';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
 import { CanActivate } from '@angular/router';
-import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs';
 
@@ -14,8 +13,7 @@ export class AdminAuthGuard implements CanActivate{
   constructor(private auth: AuthService, private userService: UserService) { }
 
   canActivate(): Observable<boolean> {
-    return this.auth.user$
-       .switchMap(user => this.userService.get(user.uid).valueChanges())
+    return this.auth.appUser$
      .map(appUser => appUser.isAdmin);
   }
 }
--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,8 +1,11 @@
+import { UserService } from './user.service';
+import { AppUser } from './models/app-user';
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import * as firebase from 'firebase';
 import { Observable } from 'rxjs';
 import { ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +13,10 @@ import { ActivatedRoute } from '@angular/router';
 export class AuthService {
   user$: Observable<firebase.User>;
 
-  constructor(private afAuth: AngularFireAuth, private route: ActivatedRoute) {
+  constructor(
+    private userService: UserService,
+    private afAuth: AngularFireAuth, 
+    private route: ActivatedRoute) {
     this.user$ = afAuth.authState;
    }
 
@@ -22,6 +28,11 @@ export class AuthService {
 
   logout() {
     this.afAuth.signOut();
+  }
+
+  get appUser$() : Observable<AppUser> {
+    return this.user$
+      .switchMap(user => this.userService.get(user.uid).valueChanges())
   }
 
 }
