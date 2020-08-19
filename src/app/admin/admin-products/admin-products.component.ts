3814bbd84fd047c4edd7c64968eb26cf5f5251ad
Prashanth <prashanthdkumar88@gmail.com>
2020-08-19 15:00:03 -0400

    Extracting a Product Interface

--- src/app/admin/admin-products/admin-products.component.ts
+++ src/app/admin/admin-products/admin-products.component.ts
@@ -1,7 +1,6 @@
 import { ProductService } from './../../product.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Subscription } from 'rxjs';
-import { filter } from 'rxjs/operators';
 import { Product } from 'src/app/models/product';
 
 @Component({
@@ -11,26 +10,21 @@ import { Product } from 'src/app/models/product';
 })
 export class AdminProductsComponent implements OnInit, OnDestroy {
   filteredProducts;
-  products;
+  products: Product[];
   subscription: Subscription;
-  searchText = '';
 
   constructor(private productService: ProductService) {
   }
 
   filter(query: string) {
-    // console.log(query);
     this.filteredProducts = (query) ?
       Object.values(this.products).filter((p: Product) => p.title.toLowerCase().includes(query.toLowerCase())) :
       this.products;
   }
 
   ngOnInit(): void {
-    // this.products$ = this.productService.getAll();
-    // this.products$.subscribe(data => {
-    // this.products = data;
-    // });
-    this.subscription =  this.productService.getAll().subscribe(prod => this.filteredProducts =  this.products = prod);
+    this.subscription =  this.productService.getAll()
+    .subscribe((prod: Product[]) => this.filteredProducts =  this.products = prod);
   }
 
   ngOnDestroy() {
