fc2e430ea779546f4d371476e7625bca47187b67
Prashanth <prashanthdkumar88@gmail.com>
2020-09-04 10:25:55 -0400

    Implementing the Change Quantity Buttons

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -37,14 +37,21 @@ export class ShoppingCartService {
   }
 
   async addToCart(product: Product) {
+    this.updateItemQuantity(product, 1);
+  }
+
+  async removeFromCart(product: Product) {
+    this.updateItemQuantity(product, -1);
+  }
+
+  private async updateItemQuantity(product: Product, change: number) {
     const cartId = await this.getOrCreateCartId();
     const item$: any =  this.getItem(cartId, product.key);
     this.productService.get(product.key).valueChanges().subscribe(prodData => {
       item$.valueChanges()
       .take(1).subscribe((item: any) => {
-          item$.update({ product: prodData, quantity: (item?.quantity || 0) + 1 });
+          item$.update({ product: prodData, quantity: (item?.quantity || 0) + change });
        });
-
     });
   }
 }
