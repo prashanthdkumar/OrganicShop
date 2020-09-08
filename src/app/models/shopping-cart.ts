fec552075a86b8635f265ebc5888c61f275cfb19
Prashanth <prashanthdkumar88@gmail.com>
2020-09-08 00:07:18 -0400

    Fixing a Design Issue on accessing shopping cart items array

--- src/app/models/shopping-cart.ts
+++ src/app/models/shopping-cart.ts
@@ -1,16 +1,18 @@
 import { ShoppingCartItem } from './shopping-cart-item';
 
 export class ShoppingCart {
-    constructor(public items: ShoppingCartItem[]) {}
+    items: ShoppingCartItem[] = [];
 
-    get productIds() {
-        return Object.keys(this.items);
+    constructor(public itemsMap: { [productId: string]: ShoppingCartItem }) {
+        for (let productId in itemsMap) {
+            this.items.push(itemsMap[productId]);
+        }
     }
 
     get totalItemsCount() {
         let count = 0;
-        for (const productId in this.items) {
-            count += this.items[productId].quantity;
+        for (const productId in this.itemsMap) {
+            count += this.itemsMap[productId].quantity;
         }
         return count;
     }
