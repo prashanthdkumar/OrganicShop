d9b3e850939663b47aea4d7f73e7bc48802235e3
Prashanth <prashanthdkumar88@gmail.com>
2020-09-19 22:17:19 -0400

    Saving the Order in Firebase and Associating the Order with the Current User

--- src/app/check-out/check-out.component.ts
+++ src/app/check-out/check-out.component.ts
@@ -1,14 +1,56 @@
-import { Component } from '@angular/core';
+import { AuthService } from './../auth.service';
+import { OrderService } from './../order.service';
+import { Subscription } from 'rxjs';
+import { ShoppingCartService } from './../shopping-cart.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ShoppingCart } from '../models/shopping-cart';
 
 @Component({
   selector: 'app-check-out',
   templateUrl: './check-out.component.html',
   styleUrls: ['./check-out.component.css']
 })
-export class CheckOutComponent {
+export class CheckOutComponent implements OnInit, OnDestroy{
   shipping: {[key: string]: string} = {};
+  cart: ShoppingCart;
+  userId: string;
+  cartSubscription: Subscription;
+  userSubscription: Subscription;
+
+
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private shoppingCartService: ShoppingCartService) {}
+
+  async ngOnInit() {
+    const cart$ = await this.shoppingCartService.getCart();
+    this.cartSubscription = cart$.subscribe(cart => this.cart = cart);
+    this.userSubscription = this.authService.user$.subscribe(user => this.userId = user.uid);
+  }
+
+  ngOnDestroy(): void {
+    this.cartSubscription.unsubscribe();
+    this.userSubscription.unsubscribe();
+  }
 
   placeOrder() {
-    console.log(this.shipping);
+    const order = {
+      userId: this.userId,
+      datePlaced: new Date().getTime(),
+      shipping: this.shipping,
+      items: this.cart.items.map(i => {
+        return {
+          product: {
+            title: i.title,
+            imageUrl: i.imageUrl,
+            price: i.price
+          },
+          quantity: i.quantity,
+          totalPrice: i.totalPrice
+        };
+      })
+    };
+    this.orderService.storeOrder(order);
   }
 }
---
+++ src/app/order.service.ts
@@ -0,0 +1,15 @@
+import { AngularFireDatabase } from '@angular/fire/database';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OrderService {
+
+  constructor(private db: AngularFireDatabase) { }
+
+  storeOrder(order) {
+    this.db.list('/orders').push(order);
+  }
+
+}
